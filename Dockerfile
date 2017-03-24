4fb6dc8930fea13152623381755e190f08f0ae22
PIFFARETTI Ludovic IMT/OLPS <LAIP7450@orange.com>
2017-03-24 16:35:43 +0100

    update

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM registry.access.redhat.com/rhel7:latest
+
+COPY oc /usr/bin/
+RUN chmod 777 /usr/bin/oc 
+RUN chgrp -R 0 /usr/bin && chmod -R g+rwX /usr/bin
+
+COPY oc.sh /tmp/oc.sh
+
+RUN chmod 777 /tmp/oc.sh
+
+CMD /tmp/oc.sh
---
+++ oc.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+export KUBECONFIG=/tmp/.kube
+
+set -e
+
+DEFAULT_NAMESPACE=$(eval cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+
+oc login https://$KUBERNETES_PORT_443_TCP_ADDR:$KUBERNETES_SERVICE_PORT_HTTPS \
+  --token `cat /var/run/secrets/kubernetes.io/serviceaccount/token` \
+  --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+oc get jobs -n ${DEFAULT_NAMESPACE} > /tmp/jobs
+tail -n +2 /tmp/jobs > /tmp/jobs-without-header
+
+while read JOB_WITH_STATS
+do
+    JOB=$(eval echo "${JOB_WITH_STATS}" | awk '{print $1}')
+    SUCCESSFUL_RUN=$(eval echo "${JOB_WITH_STATS}" | awk '{print $3}')
+    if [ ${SUCCESSFUL_RUN} == "1" ]; then
+      echo "Successfully ended job \"${JOB}\", delete it"
+      oc delete job ${JOB} -n ${DEFAULT_NAMESPACE}
+    else
+      echo "\"${JOB}\" not successfully ended"
+    fi
+done < /tmp/jobs-without-header
