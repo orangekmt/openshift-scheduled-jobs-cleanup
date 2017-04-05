182c5fef4e92234be2d4ebc599f7ac33d13196c7
ludopif <ludovic.piffaretti@orange.com>
2017-04-05 15:10:41 +0200

    Update oc.sh

--- oc.sh
+++ oc.sh
@@ -19,7 +19,7 @@ do
     SUCCESSFUL_RUN=$(eval echo "${JOB_WITH_STATS}" | awk '{print $3}')
 
     if [ ${SUCCESSFUL_RUN} == "1" ]; then
-      START=`oc get job -n abcd1234 -o yaml | grep -i "startTime:" | awk '{print $2}'`
+      START=`oc get job -n ${DEFAULT_NAMESPACE} -o yaml | grep -i "startTime:" | awk '{print $2}'`
       START_TIMESTAMP=`date -d '$START' +%s`
       CURRENT_DATE=`date +%s`
       echo "start:" $START_TIMESTAMP
