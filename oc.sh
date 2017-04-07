4e8ba760a6877655ca050ff56ae9d65c131e1641
orangekmt <kmt.team@orange.com>
2017-04-07 16:50:55 +0200

    Update oc.sh

--- oc.sh
+++ oc.sh
@@ -9,6 +9,7 @@ DEFAULT_NAMESPACE=$(eval cat /var/run/secrets/kubernetes.io/serviceaccount/names
 oc login https://$KUBERNETES_PORT_443_TCP_ADDR:$KUBERNETES_SERVICE_PORT_HTTPS \
   --token `cat /var/run/secrets/kubernetes.io/serviceaccount/token` \
   --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+
 oc get jobs -n ${DEFAULT_NAMESPACE} > /tmp/jobs
 tail -n +2 /tmp/jobs > /tmp/jobs-without-header
 
@@ -16,10 +17,23 @@ while read JOB_WITH_STATS
 do
     JOB=$(eval echo "${JOB_WITH_STATS}" | awk '{print $1}')
     SUCCESSFUL_RUN=$(eval echo "${JOB_WITH_STATS}" | awk '{print $3}')
+
     if [ ${SUCCESSFUL_RUN} == "1" ]; then
-      echo "Successfully ended job \"${JOB}\", delete it"
-      oc delete job ${JOB} -n ${DEFAULT_NAMESPACE}
+      echo "Checking Job:" $JOB
+      START=`oc get job $JOB -n ${DEFAULT_NAMESPACE} -o yaml | grep -i "startTime:" | awk '{print $2}'`
+      START_TIMESTAMP=`date -d $START +%s`
+      CURRENT_DATE=`date +%s`
+      DURATION=`expr $CURRENT_DATE - $START_TIMESTAMP`
+      echo "duration calculated:" $DURATION
+      echo "duration set by user:" $1 
+        if [[ $DURATION -gt $1 ]] ;
+          then
+            echo "Successfully ended job \"${JOB}\", delete it"
+            oc delete job ${JOB} -n ${DEFAULT_NAMESPACE}
+          else
+            echo "not to delete"
+          fi
     else
-      echo "\"${JOB}\" not successfully ended"
+      echo "\"${JOB}\" not successfully ended, not to delete"
     fi
 done < /tmp/jobs-without-header
